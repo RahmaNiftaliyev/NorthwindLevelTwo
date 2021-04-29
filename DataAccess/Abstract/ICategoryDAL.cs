d62380990b7e4248dc417be712886540bf7dacff
RahmaNiftaliyev <58683199+RahmaNiftaliyev@users.noreply.github.com>
2021-04-29 08:15:03 -0700

    Add project files.

---
+++ DataAccess/Abstract/ICategoryDAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+using Core.DataAccess.Abstract;
+
+namespace DataAccess.Abstract
+{
+    public interface ICategoryDAL : IEntityRepository<Category>
+    {
+    }
+}
---
+++ DataAccess/Abstract/IProductDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+using Core.DataAccess.Abstract;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDAL:IEntityRepository<Product>
+    {
+       //Repository Pattern
+        //listing
+        //adding
+        //deleting
+        //updating
+    }
+}
